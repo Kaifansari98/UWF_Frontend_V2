b998432f89c2b32eca67eb9964bbce79244d8282
CrestWebStudios <154508851+Kaifansari98@users.noreply.github.com>
2026-07-23 13:58:42 +0530

    chore: update dependencies including Next.js, axios, and dompurify while refining dependency overrides

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2Fp9x4icY4hbNoDiZGhu0FSBUTWHk/L2Iqg1Fl73gC0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
